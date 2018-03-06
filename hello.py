45c74764f59d2d904fc46c71f568e92b8c3cdb6e
zlt-shadow <zlt-shadow@163.com>
2018-03-06 21:30:48 +0800

    2a+2b

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print('hello, flask')
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return '<h1>Hello Flask</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, %s</h1>' % name
+
+if __name__ == '__main__':
+	app.run(debug=True)
