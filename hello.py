e145a0be7b02100217b6b331d11b689f6d95a12e
zlt-shadow <zlt-shadow@163.com>
2018-03-07 18:14:57 +0800

    use redirect() and session

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 #初始化，创建Flask类对象，也就是程序实例
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -26,9 +26,9 @@ def index():
 	name = None
 	form = NameForm()
 	if form.validate_on_submit():
-		name = form.name.data
-		form.name.data = ''
-	return render_template("index.html", form=form, name=name)
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template("index.html", form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
