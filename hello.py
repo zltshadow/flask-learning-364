b7c3e30600d5e5b1146c1b1b8e78e432f9dfeba1
zlt-shadow <zlt-shadow@163.com>
2018-03-07 11:55:45 +0800

    use render_template

--- hello.py
+++ hello.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+#初始化，创建Flask类对象，也就是程序实例
 app = Flask(__name__)
 
-from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+#从flask.ext命名空间导入Flask-Bootstrap，然后将程序实例传入构造方法进行初始化
+bootstrap = Bootstrap(app)
 
-manager = Manager(app)
-#...
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
 
 if __name__=='__main__':
-	manager.run()
+	app.run()
