88fe47f63bb9582f9d2bc0b3d74f8dda66292fc9
zlt-shadow <zlt-shadow@163.com>
2018-03-07 15:31:08 +0800

    use Flask-Moment

--- hello.py
+++ hello.py
@@ -6,9 +6,15 @@ from flask.ext.bootstrap import Bootstrap
 #从flask.ext命名空间导入Flask-Bootstrap，然后将程序实例传入构造方法进行初始化
 bootstrap = Bootstrap(app)
 
+from flask_moment import Moment
+#从flask_moment命名空间导入Flask-Moment，3.6.4版本用flask.ext会有警告，将程序实例传入构造方法进行初始化
+moment = Moment(app)
+
+from datetime import datetime
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
