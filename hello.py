eb1a14108bdfb0e5e3dea8eeedff2dabb6f2fc74
zlt-shadow <zlt-shadow@163.com>
2018-03-07 18:30:14 +0800

    use flash

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 #初始化，创建Flask类对象，也就是程序实例
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -23,9 +23,11 @@ from datetime import datetime
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	name = None
 	form = NameForm()
 	if form.validate_on_submit():
+		old_name = session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
 		session['name'] = form.name.data
 		return redirect(url_for('index'))
 	return render_template("index.html", form=form, name=session.get('name'))
