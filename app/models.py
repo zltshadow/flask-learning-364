4155fd1f1705824221c91f93ac8c67f127dc14f1
zltshadow <zlt_shadow@163.com>
2018-03-15 22:07:44 +0800

    show followed blog posts in home page

--- app/models.py
+++ app/models.py
@@ -242,11 +242,12 @@ class User(UserMixin, db.Model):
             return False
         return self.followers.filter_by(
             follower_id=user.id).first() is not None
-
-    @perperty
+    
+    @property
     def followed_posts(self):
-        return Post.query.join(Follow, Follow.followed_id == Post.author.id)\
+        return Post.query.join(Follow, Follow.followed_id == Post.author_id)\
             .filter(Follow.follower_id == self.id)
+
 
     def __repr__(self):
         return '<User %r>' % self.username
